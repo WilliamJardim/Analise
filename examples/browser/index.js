04f6de3b5b05e63456764e4c4351974b7d492f6e
William Jardim <williamalvesjardim@gmail.com>
2024-11-25 14:03:34 -0300

    Colocando exemplo mais leve

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,1 +1,6 @@
-var dataset = Analise.DataStructure( datasetExemplo );
+//var dataset = Analise.DataStructure( datasetExemplo );
+var dataset = Analise.DataStructure([
+    [1, 2,  3,  4 ],
+    [5, 6,  7,  8 ],
+    [9, 10, 11, 12]
+]);
