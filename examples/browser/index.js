55e041c2d1841c4cf1b392a7dfe3b88c36751f9e
William Jardim <williamalvesjardim@gmail.com>
2024-11-25 13:54:05 -0300

    Subindo novo ajuste

---
+++ examples/browser/index.js
@@ -0,0 +1,1 @@
+var dataset = Analise.DataStructure( datasetExemplo );
--- src/DataStructure.js
+++ src/DataStructure.js
@@ -1,5 +1,17 @@
 Analise.DataStructure = function( config={} ){
     const context = Analise.Base(config);
 
+    /**
+    * Extrai as linhas
+    * @param {*} linhaInicial 
+    * @param {*} linhaFinal 
+    * @returns 
+    */
+    context.lineRange = function(linhaInicial, linhaFinal){
+        return context.slice(linhaInicial, linhaFinal);
+    }
+
+    
+
     return context;
 }
